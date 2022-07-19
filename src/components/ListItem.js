a422f16fdf7edb76f4ca60161d84696181edec96
User <user@Users-iMac.local>
2022-07-19 12:21:37 -0400

    First commit

---
+++ src/components/ListItem.js
@@ -0,0 +1,44 @@
+import React from 'react'
+import {Link} from 'react-router-dom'
+
+
+let getDate = (note) => {
+    return new Date(note.updated).toLocaleDateString()
+}
+
+let getTitle = (note) => {
+    const title = note.body.split('\n')[0]
+
+    if(title.length > 45){
+        return title.slice(0, 45)
+    }
+    return title
+}
+
+let getContent =(note) => {
+    let title = getTitle(note)
+    let content = note.body.replaceAll('\n', ' ')
+    content = content.replaceAll(title, '')
+    if(content.length > 45){
+        content.slice(0, 45)
+    }else{
+        return content
+    }
+}
+
+const ListItem = ({note}) => {
+
+  return (
+    <div>
+        <Link to={`/note/${note.id}`}>
+            <div className='notes-list-item'>
+                <h3>{getTitle(note)}</h3>
+               <p><span>{getDate(note)}</span>{getContent(note)}</p>
+            </div>
+            
+        </Link>
+    </div>
+  )
+}
+
+export default ListItem
